d0920027c3bb7c4446e32cf6b6af4460098f6db2
aspgln <qdai@ucla.edu>
2026-08-09 21:00:31 +0000

    Deploying to gh-pages from @ aspgln/aspgln.github.io@bcea6ec59c4569ba4fe59b6eb784df6c78a4b5a0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-publications",
           title: "Publications",
-          description: "Peer-reviewed journal publications and conference abstracts.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
